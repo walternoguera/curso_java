eaae1a79aa22968504bd70d06ba6c2219271e8f8
walternoguera <walter.morel.noguera@gmail.com>
2025-04-02 22:53:52 +0200

    prueba

---
+++ ejemplo_netbeans/src/ejemplo_netbeans/Ejemplo_netbeans.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplo_netbeans;
+
+/**
+ *
+ * @author Walter
+ */
+public class Ejemplo_netbeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
